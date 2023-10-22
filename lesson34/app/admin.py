a106e01eb266ed593aaf42cea9c7e97d65a15bcb
Sam23656 <sem26m06@gmail.com>
2023-10-22 18:06:18 +0500

    Add Like, Dislike

---
+++ lesson34/app/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+
+from app.models import *
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Cart)
+admin.site.register(BuyHistory)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Dislike)
---
+++ lesson34/app/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from app.models import Comment
+
+
+class CommentModelForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('body', )
